6632012bbcc7c8df15485b64ef5c8b807484b912
humano Power <elcorreodeyamil@hotmail.com>
2019-09-03 03:16:18 +0000

    Se agrega un ligero cambio de estructura en la entidad sin cambio en la funcionalidad

--- SIIC.Ordenes/Infrastructure.Data.Interface/IDataEntity.cs
+++ SIIC.Ordenes/Infrastructure.Data.Interface/IDataEntity.cs
@@ -6,11 +6,24 @@ namespace Infrastructure.Data.Interface
 {
 	public interface IDataEntity
 	{
+		/// <summary>
+		/// MEtodo que devuelve un listado  de tipo objeto, que tendrá que aplicarle un cast para poder revisar contenido.
+		/// </summary>
+		/// <returns></returns>
 		IEnumerable<object> Get();
 	
-
+		/// <summary>
+		/// MEtodo que permite agregar un objeto a la base de datos
+		/// </summary>
 		void Add();
+		/// <summary>
+		/// Metodo que permite eliminar un objeto en la persistencia de la base de datos.
+		/// </summary>
+		
 		void Delete();
+		/// <summary>
+		/// Metodo que permite actualizar un registro en la base de datos.
+		/// </summary>
 		void Update();
 	}
 }
